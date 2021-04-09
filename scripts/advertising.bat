391c202870c4863bdb0c2a12984a1a21486f9838
Whayn <64968114+whayn@users.noreply.github.com>
2021-04-09 15:50:38 +0200

    Update advertising.bat

--- scripts/advertising.bat
+++ scripts/advertising.bat
@@ -18,9 +18,9 @@ cls
 echo Edit here
 echo Y Yes !
 echo N No thanks.
-set /p answer=choice Y or N
-if %answer%==Y (goto Correct) else (goto answer2)
-if %answer%==N (goto Incorrect) else (goto answer2)
+set /p choice=choice Y or N
+if %choice%==Y (goto Correct) 
+if %choice%==N (goto Incorrect) else (goto answer2)
 :answer2
 cls
 echo You need to answer Y or N
@@ -30,9 +30,9 @@ cls
 echo Edit here
 echo Y Yes !
 echo N No thanks.
-set /p answer=choice Y or N
-if %answer%==Y (goto Correct) else (goto answer)
-if %answer%==N (goto Incorrect) else (goto answer)
+set /p choice=choice Y or N
+if %choice%==Y (goto Correct) 
+if %choice%==N (goto Incorrect) else (goto answer2)
 :Correct
 cls
 :: enter a cool text
@@ -47,11 +47,16 @@ echo Thanks for your support
 :: enter link here \/
 start https://www.whayn.tk/
 pause
-pause
+goto end
 :Incorrect
 cls
 :: enter a cool text
 echo Okay, I understand your choice
 pause
+pause
+pause
+goto end
+:end
+echo See you soon
 pause
 pause
